f96dd0f690c279e3ffff384dec9d1cce98faebf5
Pxny <95312348+Pxny@users.noreply.github.com>
2021-12-01 12:58:13 -0500

    Deck of Cards Added
    1

--- index.php
+++ index.php
@@ -1,5 +1,62 @@
 <?php 
-    // php
+    function LoadDeck() {
+        $cards = array(
+            array("H2", 2, "Deuce"),
+            array("H3", 3, "Three"),
+            array("H4", 4, "Four"),
+            array("H5", 5, "Five"),
+            array("H6", 6, "Six"),
+            array("H7", 7, "Seven"),
+            array("H8", 8, "Eight"),
+            array("H9", 9, "Nine"),
+            array("H10", 10, "Ten"),
+            array("H11", 11, "Jack"),
+            array("H12", 12, "Queen"),
+            array("H13", 13, "King"),
+            array("H14", 14, "Ace"),
+            array("D2", 2, "Deuce"),
+            array("D3", 3, "Three"),
+            array("D4", 4, "Four"),
+            array("D5", 5, "Five"),
+            array("D6", 6, "Six"),
+            array("D7", 7, "Seven"),
+            array("D8", 8, "Eight"),
+            array("D9", 9, "Nine"),
+            array("D10", 10, "Ten"),
+            array("D11", 11, "Jack"),
+            array("D12", 12, "Queen"),
+            array("D13", 13, "King"),
+            array("D14", 14, "Ace"),
+            array("C2", 2, "Deuce"),
+            array("C3", 3, "Three"),
+            array("C4", 4, "Four"),
+            array("C5", 5, "Five"),
+            array("C6", 6, "Six"),
+            array("C7", 7, "Seven"),
+            array("C8", 8, "Eight"),
+            array("C9", 9, "Nine"),
+            array("C10", 10, "Ten"),
+            array("C11", 11, "Jack"),
+            array("C12", 12, "Queen"),
+            array("C13", 13, "King"),
+            array("C14", 14, "Ace"),
+            array("S2", 2, "Deuce"),
+            array("S3", 3, "Three"),
+            array("S4", 4, "Four"),
+            array("S5", 5, "Five"),
+            array("S6", 6, "Six"),
+            array("S7", 7, "Seven"),
+            array("S8", 8, "Eight"),
+            array("S9", 9, "Nine"),
+            array("S10", 10, "Ten"),
+            array("S11", 11, "Jack"),
+            array("S12", 12, "Queen"),
+            array("S13", 13, "King"),
+            array("S14", 14, "Ace"),
+        );
+        return $cards;
+    }
+
 ?>
 
 <html>
